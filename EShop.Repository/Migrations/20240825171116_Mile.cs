2e12a8f97ef80aaa403b9caa1491a2a205dfa177
MileGjorgiev <125594388+MileGjorgiev@users.noreply.github.com>
2024-09-01 18:14:21 +0200

    Add files via upload

---
+++ EShop.Repository/Migrations/20240825171116_Mile.cs
@@ -0,0 +1,240 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EShop.Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class Mile : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_MatchSchedules_Team_Team1Id",
+                table: "MatchSchedules");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_MatchSchedules_Team_Team2Id",
+                table: "MatchSchedules");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Player_Team_TeamId",
+                table: "Player");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_PlayerOnTeam_Player_PlayerId",
+                table: "PlayerOnTeam");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_PlayerOnTeam_Team_TeamId",
+                table: "PlayerOnTeam");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Team",
+                table: "Team");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_PlayerOnTeam",
+                table: "PlayerOnTeam");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Player",
+                table: "Player");
+
+            migrationBuilder.RenameTable(
+                name: "Team",
+                newName: "Teams");
+
+            migrationBuilder.RenameTable(
+                name: "PlayerOnTeam",
+                newName: "PlayerOnTeams");
+
+            migrationBuilder.RenameTable(
+                name: "Player",
+                newName: "Players");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_PlayerOnTeam_TeamId",
+                table: "PlayerOnTeams",
+                newName: "IX_PlayerOnTeams_TeamId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_PlayerOnTeam_PlayerId",
+                table: "PlayerOnTeams",
+                newName: "IX_PlayerOnTeams_PlayerId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Player_TeamId",
+                table: "Players",
+                newName: "IX_Players_TeamId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Teams",
+                table: "Teams",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_PlayerOnTeams",
+                table: "PlayerOnTeams",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Players",
+                table: "Players",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_MatchSchedules_Teams_Team1Id",
+                table: "MatchSchedules",
+                column: "Team1Id",
+                principalTable: "Teams",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_MatchSchedules_Teams_Team2Id",
+                table: "MatchSchedules",
+                column: "Team2Id",
+                principalTable: "Teams",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PlayerOnTeams_Players_PlayerId",
+                table: "PlayerOnTeams",
+                column: "PlayerId",
+                principalTable: "Players",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PlayerOnTeams_Teams_TeamId",
+                table: "PlayerOnTeams",
+                column: "TeamId",
+                principalTable: "Teams",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Players_Teams_TeamId",
+                table: "Players",
+                column: "TeamId",
+                principalTable: "Teams",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_MatchSchedules_Teams_Team1Id",
+                table: "MatchSchedules");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_MatchSchedules_Teams_Team2Id",
+                table: "MatchSchedules");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_PlayerOnTeams_Players_PlayerId",
+                table: "PlayerOnTeams");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_PlayerOnTeams_Teams_TeamId",
+                table: "PlayerOnTeams");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Players_Teams_TeamId",
+                table: "Players");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Teams",
+                table: "Teams");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Players",
+                table: "Players");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_PlayerOnTeams",
+                table: "PlayerOnTeams");
+
+            migrationBuilder.RenameTable(
+                name: "Teams",
+                newName: "Team");
+
+            migrationBuilder.RenameTable(
+                name: "Players",
+                newName: "Player");
+
+            migrationBuilder.RenameTable(
+                name: "PlayerOnTeams",
+                newName: "PlayerOnTeam");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Players_TeamId",
+                table: "Player",
+                newName: "IX_Player_TeamId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_PlayerOnTeams_TeamId",
+                table: "PlayerOnTeam",
+                newName: "IX_PlayerOnTeam_TeamId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_PlayerOnTeams_PlayerId",
+                table: "PlayerOnTeam",
+                newName: "IX_PlayerOnTeam_PlayerId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Team",
+                table: "Team",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Player",
+                table: "Player",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_PlayerOnTeam",
+                table: "PlayerOnTeam",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_MatchSchedules_Team_Team1Id",
+                table: "MatchSchedules",
+                column: "Team1Id",
+                principalTable: "Team",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_MatchSchedules_Team_Team2Id",
+                table: "MatchSchedules",
+                column: "Team2Id",
+                principalTable: "Team",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Player_Team_TeamId",
+                table: "Player",
+                column: "TeamId",
+                principalTable: "Team",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PlayerOnTeam_Player_PlayerId",
+                table: "PlayerOnTeam",
+                column: "PlayerId",
+                principalTable: "Player",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PlayerOnTeam_Team_TeamId",
+                table: "PlayerOnTeam",
+                column: "TeamId",
+                principalTable: "Team",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ EShop.Repository/Migrations/20240831005216_FixMatchSch.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EShop.Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixMatchSch : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_MatchSchedules_Teams_Team1Id",
+                table: "MatchSchedules");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_MatchSchedules_Teams_Team2Id",
+                table: "MatchSchedules");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Team2Id",
+                table: "MatchSchedules",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Team1Id",
+                table: "MatchSchedules",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_MatchSchedules_Teams_Team1Id",
+                table: "MatchSchedules",
+                column: "Team1Id",
+                principalTable: "Teams",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_MatchSchedules_Teams_Team2Id",
+                table: "MatchSchedules",
+                column: "Team2Id",
+                principalTable: "Teams",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_MatchSchedules_Teams_Team1Id",
+                table: "MatchSchedules");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_MatchSchedules_Teams_Team2Id",
+                table: "MatchSchedules");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Team2Id",
+                table: "MatchSchedules",
+                type: "uniqueidentifier",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Team1Id",
+                table: "MatchSchedules",
+                type: "uniqueidentifier",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_MatchSchedules_Teams_Team1Id",
+                table: "MatchSchedules",
+                column: "Team1Id",
+                principalTable: "Teams",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_MatchSchedules_Teams_Team2Id",
+                table: "MatchSchedules",
+                column: "Team2Id",
+                principalTable: "Teams",
+                principalColumn: "Id");
+        }
+    }
+}
